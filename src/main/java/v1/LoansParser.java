874f900ddda2fd0e5c38da01affd168d479b136e
davebaiFromPrintersRow <general.dave.bai@gmail.com>
2017-08-26 17:47:04 -0500

    add junit to count column headers

--- src/main/java/v1/LoansParser.java
+++ src/main/java/v1/LoansParser.java
@@ -1,7 +1,37 @@
 package v1;
 
-public class LoansParser {
-    public LoansParser(String loansFile) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Optional;
 
+public class LoansParser {
+    private Optional<Integer> columnCount = Optional.empty();
+
+    public LoansParser(String loansFile, boolean hasHeader, boolean tabSeparated) throws IOException {
+        if (!tabSeparated) {
+            throw new InvalidIncomingDataFileFormatException();
+        }
+        InputStream resourceAsStream = this.getClass().getClassLoader().getResourceAsStream(loansFile);
+        BufferedReader in = new BufferedReader(new InputStreamReader(resourceAsStream));
+        String line = null;
+
+        while ((line = in.readLine()) != null) {
+            if (hasHeader) {
+                String[] splitHeaderTabs = line.split("\t");
+                this.columnCount = Optional.of(splitHeaderTabs.length);
+                hasHeader = false;
+            } else {
+
+            }
+        }
+    }
+
+    public Optional<Integer> getColumnCount() {
+        return columnCount;
+    }
+
+    public class InvalidIncomingDataFileFormatException extends RuntimeException {
     }
 }
--- src/test/java/v1/LoansParserTest.java
+++ src/test/java/v1/LoansParserTest.java
@@ -2,9 +2,20 @@ package v1;
 
 import org.junit.Test;
 
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
 public class LoansParserTest {
+
+    @Test(expected = LoansParser.InvalidIncomingDataFileFormatException.class)
+    public void testParserThrowsException_WhenNonTabSeparatedExceptionIsUsed_ThenExceptionThrown() throws Exception {
+        new LoansParser("LoansInfo", true, false);
+    }
+
     @Test
     public void testParserModelsFile_WhenFileIsPassedToPaser_ThenTheObjectCanProvideSchemaInformation() throws Exception {
-        LoansParser loansParser = new LoansParser("LoansInfo");
+        LoansParser loansParser = new LoansParser("LoansInfo", true, true);
+        assertEquals(Optional.of(12), loansParser.getColumnCount());
     }
 }
