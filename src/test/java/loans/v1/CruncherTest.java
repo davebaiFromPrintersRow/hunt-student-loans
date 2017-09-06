4d215d5a40dc26cd092aa20fd9fcd8e43dedece3
davebaiFromPrintersRow <general.dave.bai@gmail.com>
2017-09-05 22:54:56 -0500

    change perkins principal to correct amount

--- src/test/java/loans/v1/CruncherTest.java
+++ src/test/java/loans/v1/CruncherTest.java
@@ -8,7 +8,7 @@ public class CruncherTest {
 
     @Test
     public void perkinsLoanAShouldBePaidOffIn120Months() throws Exception {
-        PerkinsA perkinsA = new PerkinsA(400);
+        PerkinsA perkinsA = new PerkinsA(4000);
         int monthsLeftToPay = Cruncher.getMonthsLeftToPay(perkinsA);
         assertEquals(121, monthsLeftToPay);
     }
